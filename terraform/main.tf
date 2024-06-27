fc0120b2c72ec0a2a8f8303799119e6522b06d27
root <root@DESKTOP-R6QB3SK.localdomain>
2024-06-27 15:48:35 +0530

    modifications

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,17 +9,3 @@ terraform {
   required_version = ">=0.14.9"
 
 }
-
-provider "aws" {
-  version = "~>3.0"
-  region  = "ap-south-1"
-}
-
-required_version = ">=0.14.9" 
-
-   backend "s3" {
-       bucket = "[Remote_State_S3_Bucket_Name]"
-       key    = "[Remote_State_S3_Bucket_Key]"
-       region = "ap-south-1"
-   }
-}
